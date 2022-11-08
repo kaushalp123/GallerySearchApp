151aba69b6eeb9d66878ac52f1787f03449ae35e
Kaushal <kaushal.p@e9ine.com>
2022-11-08 22:54:26 +0530

    perf: better handling of API exceptions

--- app/src/main/java/com/kaushal/galleryapp/util/APIErrorHandler.kt
+++ app/src/main/java/com/kaushal/galleryapp/util/APIErrorHandler.kt
@@ -2,15 +2,17 @@ package com.kaushal.galleryapp.util
 
 import android.accounts.NetworkErrorException
 import android.content.Context
+import com.squareup.moshi.JsonDataException
 import retrofit2.HttpException
 
-class APIErrorHandler(private val context: Context) {
+class APIErrorHandler() {
 
     // this function accepts the Throwable type as an argument and provides the appropriate error message by checking its type, default error message is returned otherwise.
     fun errMessage(error: Throwable) : String {
         return when (error) {
-            is HttpException -> "API Exception"
-            is NetworkErrorException -> "Please check your internet connection"
+            is HttpException -> "Server Error occurred!"
+            is NetworkErrorException -> "Please check your internet connection!"
+            is JsonDataException -> "There was a problem retrieving your search results!"
             else -> when {
                 !error.localizedMessage.isNullOrEmpty() -> error.localizedMessage.orEmpty()
                 !error.message.isNullOrEmpty() -> error.message.orEmpty()
