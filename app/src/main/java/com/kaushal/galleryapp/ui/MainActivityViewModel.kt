24e4cbc0a8c2c1f5014c3c18101cc29a2ef6ffc0
Kaushal <kaushal.p@e9ine.com>
2022-11-08 22:58:26 +0530

    fix: proper handling of stateFlow initial value and null value with code refactoring

--- app/src/main/java/com/kaushal/galleryapp/ui/MainActivityViewModel.kt
+++ app/src/main/java/com/kaushal/galleryapp/ui/MainActivityViewModel.kt
@@ -1,9 +1,7 @@
 package com.kaushal.galleryapp.ui
 
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.kaushal.galleryapp.data.model.Data
 import com.kaushal.galleryapp.data.model.ImageResponse
 import com.kaushal.galleryapp.data.repo.SearchImageRepository
 import com.kaushal.galleryapp.util.APIErrorHandler
@@ -11,7 +9,9 @@ import com.kaushal.galleryapp.util.Outcome
 import com.kaushal.galleryapp.util.failure
 import com.kaushal.galleryapp.util.success
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 class MainActivityViewModel constructor(
@@ -19,37 +19,31 @@ class MainActivityViewModel constructor(
     private val apiErrorHandler: APIErrorHandler
 ) : ViewModel() {
 
-/*    fun setState(clientId: String, searchedText: String, pageNo: Int){
-        stateHandle["clientId"] = clientId
-        stateHandle["searchedText"] = searchedText
-        stateHandle["pageNo"] = pageNo
-    }*/
-
-/*    private val clientId: String
-        get() = stateHandle["clientId"]
-            ?: throw IllegalArgumentException("client id is missing!")
-
-    private val searchedText: String
-        get() = stateHandle["searchedText"]
-            ?: throw IllegalArgumentException("Search text is empty!")
-
-    private val pageNo: Int
-        get() = stateHandle["pageNo"]!!*/
-
     private val _searchedResult = MutableStateFlow<Outcome<ImageResponse>>(Outcome.Empty)
-    val searchedResult = _searchedResult.asStateFlow()
+    val searchedResult = _searchedResult.mapLatest {
+        if (it is Outcome.Empty) null else it
+    }.stateIn(viewModelScope, started = SharingStarted.Lazily, initialValue = null)
 
     fun fetchImages(clientId: String, searchedText: String?, pageNo: Int) {
 
-        if(searchedText.isNullOrEmpty()) throw IllegalArgumentException("Search text is empty!")
-
-        viewModelScope.launch {
-            try {
-                val result = searchImageRepository.getSearchedImages(pageNo, clientId, searchedText)
-                _searchedResult.success(result, result.data.isEmpty())
-            } catch (e: java.lang.Exception) {
-               _searchedResult.failure(e, apiErrorHandler.errMessage(e))
+        try {
+            if (searchedText.isNullOrEmpty()) {
+                _searchedResult.failure(
+                    java.lang.IllegalArgumentException(),
+                    "search query cannot be empty!"
+                )
+                return
             }
+
+            viewModelScope.launch {
+                try {
+                    val result = searchImageRepository.getSearchedImages(pageNo, clientId, searchedText)
+                    _searchedResult.success(result, result.data.isEmpty())
+                } catch (e: java.lang.Exception) {
+                    _searchedResult.failure(e, apiErrorHandler.errMessage(e))
+                }
+            }
+        } catch (e: Exception) {
         }
     }
 
